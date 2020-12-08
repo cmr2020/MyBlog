b9481272c93f2e2fcdde004e88955639d3263da4
rezanabhani387 <rezanabhani387@gmail.com>
2020-12-08 19:36:57 +0330

    قالب بندی پروژه

--- MyBlog.Web/Startup.cs
+++ MyBlog.Web/Startup.cs
@@ -16,6 +16,8 @@ namespace MyBlog.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,9 +29,12 @@ namespace MyBlog.Web
             }
 
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
